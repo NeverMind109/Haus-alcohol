517ba8dc6eb662f2e0bf18dcbdccc453aec272b3
NeverMind109 <vladgladkih123@gmail.com>
2022-05-23 23:14:34 +0300

    Created: Added Reviews & Goods Sections

--- src/js/components/notification.js
+++ src/js/components/notification.js
@@ -1,6 +1,8 @@
 const notification = document.querySelector(".notification");
 const notificationClose = document.querySelector(".notification__close");
+const header = document.querySelector(".header");
 
 notificationClose.addEventListener("click", () => {
   notification.style.display = "none";
+  header.style = "top: 0";
 });
