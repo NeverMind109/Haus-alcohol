2a941715e8a5f06b682bf3d7da903e28c171e917
NeverMind109 <vladgladkih123@gmail.com>
2022-05-24 19:13:55 +0300

    Created: Added Adaptive Response

--- src/js/components/notification.js
+++ src/js/components/notification.js
@@ -1,8 +1,13 @@
 const notification = document.querySelector(".notification");
 const notificationClose = document.querySelector(".notification__close");
 const header = document.querySelector(".header");
+const burger = document.querySelector(".burger");
 
 notificationClose.addEventListener("click", () => {
   notification.style.display = "none";
   header.style = "top: 0";
+});
+
+burger.addEventListener("click", () => {
+  header.style.zIndex = header.style.zIndex == "50" ? "10" : "50";
 });
